fe2872bd401f0ec93d1be238759077fb72041a68
sahuf2003 <sksahuf2003@gmail.com>
2025-04-27 20:28:57 +0530

    ok

---
+++ src/components/Day1.jsx
@@ -0,0 +1,9 @@
+function DAy1(){
+    return(
+        <>
+        <button className="bg-blue-600 text-white"> Primary Button</button>
+        <button className="bg-gray-200 text-black"> Secondary Button </button>
+        <button className="bg-red-300"> </button>
+        </>
+    )
+}
